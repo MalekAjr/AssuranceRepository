a4d5d1fbb89089753ed87eed1cea8a2863660839
MalekAjr <malekajroud18@gmail.com>
2026-07-30 00:13:39 +0100

    Fix CORS for production frontend

--- Desktop/SI/server/src/main.ts
+++ Desktop/SI/server/src/main.ts
@@ -5,8 +5,13 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
    app.enableCors({
-    origin: "http://localhost:5173",
-  });
+  origin: [
+    "http://localhost:5173",
+    "https://assurancefront.onrender.com",
+  ],
+  methods: "GET,POST,PUT,DELETE,PATCH,OPTIONS",
+  credentials: true,
+});
   
   await app.listen(process.env.PORT ?? 3000);
 }
